9ad00b70d0bf3c190808051147359f1e5138fbdf
hienduc14 <hiennguyenduc2k5@gmail.com>
2024-03-08 16:38:46 +0700

    Them bien rectst de xac dinh ro viec rendercopy

--- BigObj.cpp
+++ BigObj.cpp
@@ -1,6 +1,7 @@
 #include "BigObj.hpp"
 
 BigObj::BigObj(){
+    rectst = {0, 0, 0, 0};
     rect = {0, 0, 0, 0};
     texture = nullptr;
 }
@@ -11,12 +12,16 @@ BigObj::~BigObj(){
         texture = nullptr;
     }
 }
+void BigObj::SetRectSt(int x, int y){this->rectst.x = x; this->rectst.y = y;}
+void BigObj::SetRect(int x, int y){this->rect.x = x; this->rect.y = y;}
+void BigObj::SetTexture( const char* filePath ){texture = base::Load_Image( filePath );}
+
+SDL_Rect BigObj::GetRectSt(){ return this->rectst; }
+SDL_Rect BigObj::GetRect(){ return this->rect; }
+SDL_Texture* BigObj::GetTexture(){ return this->texture; }
+
 
 void BigObj::drawObj()
 {
-    SDL_RenderCopy(base::renderer, texture, NULL, &rect);
-}
-void BigObj::SetTexture( const char* filePath )
-{
-    texture = base::Load_Image( filePath );
+    SDL_RenderCopy(base::renderer, texture, &rectst, &rect);
 }
--- BigObj.hpp
+++ BigObj.hpp
@@ -7,17 +7,20 @@ class BigObj
 protected :
     SDL_Rect rect;
     SDL_Texture* texture;
+    SDL_Rect rectst;
 public :
     // tao constructor va destructor
     BigObj();
     ~BigObj();
 
     // tao setter va getter cua rect va texture
-    void SetRect(int x, int y, int w, int h){ this->rect = {x, y, w, h}; };
-    SDL_Rect GetRect(){ return this->rect; };
-
+    void SetRectSt(int x, int y);
+    void SetRect(int x, int y);
     void SetTexture( const char* filePath );
-    SDL_Texture* GetTexture(){ return this->texture; };
+
+    SDL_Rect GetRectSt();
+    SDL_Rect GetRect();
+    SDL_Texture* GetTexture();
 
     // ve hinh anh tu texture
     void drawObj();
