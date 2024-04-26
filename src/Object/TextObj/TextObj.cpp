af87163b4453ef9c308873d41c411f32625b25a2
hienduc14 <hiennguyenduc2k5@gmail.com>
2024-04-26 21:50:27 +0700

    sua loi textobj texture

--- src/Object/TextObj/TextObj.cpp
+++ src/Object/TextObj/TextObj.cpp
@@ -12,6 +12,7 @@ TextObj::~TextObj()
 
 void TextObj::SetTTF(std::string tex, int x, int y, TTF_Font* font, SDL_Color color)
 {
+    SDL_DestroyTexture(texture);
     texture = base::Load_Text(tex, font, color);
     SDL_QueryTexture(texture, NULL, NULL, &rectst.w, &rectst.h);
     rectst.x = rectst.y = 0;
